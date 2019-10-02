3b655a2cb72042c790545a6677baf24a3093bc64
Erick Quintanar <erickquintanar.dev@gmail.com>
2019-10-02 14:49:55 -0500

    Initial Commit

---
+++ Quest_Global_Code_Challenge_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quest_Global_Code_Challenge_iOS
+//
+//  Created by Erick Quintanar on 10/2/19.
+//  Copyright © 2019 Erick Quintanar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Quest_Global_Code_Challenge_iOSTests/Quest_Global_Code_Challenge_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Quest_Global_Code_Challenge_iOSTests.swift
+//  Quest_Global_Code_Challenge_iOSTests
+//
+//  Created by Erick Quintanar on 10/2/19.
+//  Copyright © 2019 Erick Quintanar. All rights reserved.
+//
+
+import XCTest
+@testable import Quest_Global_Code_Challenge_iOS
+
+class Quest_Global_Code_Challenge_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
